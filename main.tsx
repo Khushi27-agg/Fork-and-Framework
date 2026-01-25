8a503265b112310bd2a9e91e28aed935c1bebc76
Khushi-codes <khushiaggarwal2702@gmail.com>
2026-01-26 01:31:10 +0530

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to. Ensure index.html has a <div id='root'>.");
+  throw new Error("Root element not found. Check index.html");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
